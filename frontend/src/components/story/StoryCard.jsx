d6c56f173ed9197b9fc1fbbfcb49b90de1a3eb24
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-09-19 16:46:57 +0530

    Color Vars added

--- frontend/src/components/story/StoryCard.jsx
+++ frontend/src/components/story/StoryCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { secondary } from '../../colors';
 
 function StoryCard() {
   return (
     <div className='w-full pb-4 bg-[#00000048] font-[magra] rounded-lg'>
-        <div className='bg-[#4F709C] px-2 pb-2 rounded-t-md'>
+        <div className={`bg-[${secondary}] px-2 pb-2 rounded-t-md`}>
             <p className='py-1 text-2xl uppercase font-semibold'>skylight city</p>
             <div className='flex gap-2'>
-                <div className='rounded-full bg-white w-8 h-8 mt-1'/>
+                <div className='rounded-full bg-white w-[35px] h-[35px] mt-1'/>
                 <div className='flex flex-col text-sm'>
                     <p>Harshit Yadav</p>
                     <div className='flex gap-8'>
@@ -17,16 +18,16 @@ function StoryCard() {
             </div>
         </div>
         <div className='flex gap-2 text-sm p-2'>
-            <button className='capitalize bg-[#4F709C] px-1 rounded-sm'>humour</button>
-            <button className='capitalize bg-[#4F709C] px-1 rounded-sm'>mystery</button>
+            <button className={`capitalize bg-[${secondary}] px-2 py-1 rounded-[4px]`}>humour</button>
+            <button className={`capitalize bg-[${secondary}] px-2 py-1 rounded-[4px]`}>mystery</button>
         </div>
         <p className='px-2'>Experience the Skyward Wonder of Skylight City, Where Everyone Can Fly! Explore a world where the sky is your playground and discover the magic of low-altitude flight. </p>
         <div className='px-2 mt-4 flex justify-between items-center'>
             <p className='text-sm'>12 min read</p>
-            <button className='capitalize bg-[#4F709C] px-2 rounded-[4px]'>read story</button>
+            <button className={`capitalize bg-[${secondary}] px-3 py-[2px] rounded-[5px]`}>read story</button>
             <div className='flex gap-2'>
-                <button className='bg-[#4F709C] px-2 rounded-sm'>u</button>
-                <button className='bg-[#4F709C] px-2 rounded-sm'>s</button>
+                <button className={`bg-[${secondary}] px-2 rounded-sm pb-[3px]`}>u</button>
+                <button className={`bg-[${secondary}] px-2 rounded-sm pb-[3px]`}>s</button>
             </div>
         </div>
     </div>
--- frontend/src/pages/StoriesPage.jsx
+++ frontend/src/pages/StoriesPage.jsx
@@ -8,11 +8,11 @@ function StoriesPage() {
       <div className='flex justify-between items-center'>
           <p className='text-[26px] sm:text-3xl'>Stories</p>
         <div className='flex gap-2 sm:gap-6 md:gap-24 sm:text-lg md:text-xl'>
-          <p className='py-2 px-3 bg-[#4F709C] rounded-md'><Link to={'/home/mystories'}>My Stories</Link></p>
-          <button className='py-2 px-3 bg-[#4F709C] rounded-md'>Add</button>
+          <p className={`py-2 px-3 bg-[${secondary}] rounded-md`}><Link to={'/home/mystories'}>My Stories</Link></p>
+          <button className={`py-2 px-3 bg-[${secondary}] rounded-md`}>Add</button>
         </div>
       </div>
-        <div className='flex gap-2 flex-wrap py-8'>
+        <div className='flex gap-6 flex-wrap py-8'>
           <StoryCard></StoryCard>
           <StoryCard></StoryCard>
           <StoryCard></StoryCard>
