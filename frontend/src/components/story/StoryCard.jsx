fb7adc8808580f0f5d868fe3b122bb8e826f2e7a
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-09-19 19:23:13 +0530

    Story Card Design :Desktop,Tablet Done

--- frontend/src/components/story/StoryCard.jsx
+++ frontend/src/components/story/StoryCard.jsx
@@ -3,17 +3,31 @@ import { secondary } from '../../colors';
 
 function StoryCard() {
   return (
-    <div className='w-full pb-4 bg-[#00000048] font-[magra] rounded-lg'>
-        <div className={`bg-[${secondary}] px-2 pb-2 rounded-t-md`}>
-            <p className='py-1 text-2xl uppercase font-semibold'>skylight city</p>
-            <div className='flex gap-2'>
-                <div className='rounded-full bg-white w-[35px] h-[35px] mt-1'/>
-                <div className='flex flex-col text-sm'>
-                    <p>Harshit Yadav</p>
-                    <div className='flex gap-8'>
-                        <p>17/06/23</p>
-                        <p>12:23pm</p>
-                    </div>    
+    <div className='w-full pb-4 bg-[#00000048] font-[magra] rounded-lg md:max-w-[49%]'>
+        <div className='md:flex md:justify-between md:w-full'>
+            <div className={`bg-[${secondary}] px-2 pb-2 rounded-t-md md:w-fit md:px-6 md:pb-0 md:h-fit`}>
+                <p className='py-1 text-2xl uppercase font-semibold'>skylight city</p>
+                <div className='flex gap-2 md:hidden'>
+                    <div className='rounded-full bg-white w-[35px] h-[35px] mt-1'/>
+                    <div className='flex flex-col text-sm'>
+                        <p>Harshit Yadav</p>
+                        <div className='flex gap-8'>
+                            <p>17/06/23</p>
+                            <p>12:23pm</p>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+            <div className='flex-col gap-1 hidden md:flex pt-2 pr-2'>
+                <div className='flex gap-3'>
+                    <div className='flex flex-col items-end'>
+                        <p>Harshit Yadav</p>
+                        <div className='flex gap-1'>
+                            <p>17/06/23</p>
+                            <p>12:33pm</p>
+                        </div>
+                    </div>
+                    <div className='rounded-full w-10 h-10 bg-white'/>
                 </div>
             </div>
         </div>
@@ -27,7 +41,7 @@ function StoryCard() {
             <button className={`capitalize bg-[${secondary}] px-3 py-[2px] rounded-[5px]`}>read story</button>
             <div className='flex gap-2'>
                 <button className={`bg-[${secondary}] px-2 rounded-sm pb-[3px]`}>u</button>
-                <button className={`bg-[${secondary}] px-2 rounded-sm pb-[3px]`}>s</button>
+            <button className={`bg-[${secondary}] px-2 rounded-sm pb-[3px]`}>s</button>
             </div>
         </div>
     </div>
--- frontend/src/pages/StoriesPage.jsx
+++ frontend/src/pages/StoriesPage.jsx
@@ -7,12 +7,12 @@ function StoriesPage() {
     <div className={`w-full min-h-full p-4 rounded-lg bg-[${primary}]`}>
       <div className='flex justify-between items-center'>
           <p className='text-[26px] sm:text-3xl'>Stories</p>
-        <div className='flex gap-2 sm:gap-6 md:gap-24 sm:text-lg md:text-xl'>
+        <div className='flex gap-2 sm:gap-6 md:gap-24 sm:text-lg md:text-2xl md:tracking-wider'>
           <p className={`py-2 px-3 bg-[${secondary}] rounded-md`}><Link to={'/home/mystories'}>My Stories</Link></p>
           <button className={`py-2 px-3 bg-[${secondary}] rounded-md`}>Add</button>
         </div>
       </div>
-        <div className='flex gap-6 flex-wrap py-8'>
+        <div className='flex gap-2 flex-wrap py-8 justify-between'>
           <StoryCard></StoryCard>
           <StoryCard></StoryCard>
           <StoryCard></StoryCard>
