fb90029886f64ff3d79552e0a79b11a2ce9a8c29
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-09-20 16:54:35 +0530

    Heading font size adjusted
    3xl to 4xl

--- frontend/src/components/leaderboard/LeaderBoard.jsx
+++ frontend/src/components/leaderboard/LeaderBoard.jsx
@@ -4,7 +4,7 @@ import { primary } from '../../colors'
 function LeaderBoard() {
   return (
     <div className={`min-h-full max-w-fit gap-2 px-4 py-4 flex-col rounded-lg hidden lg:flex bg-[${primary}] lg:rounded-2xl`}>
-        <p className="text-3xl w-full text-center uppercase">leaderboards</p>
+        <p className="text-4xl w-full text-center uppercase">leaderboards</p>
         <div className="flex flex-col gap-2 pt-8 w-full">
             <LeaderBoardCard/>
             <LeaderBoardCard/>
--- frontend/src/pages/StoriesPage.jsx
+++ frontend/src/pages/StoriesPage.jsx
@@ -9,7 +9,7 @@ function StoriesPage() {
       <div className="flex w-full gap-10 justify-between">
         <div className={`grow w-full p-2 rounded-lg bg-[${primary}] lg:py-4 lg:px-8 lg:rounded-2xl`}>
           <div className="w-full flex justify-between items-center">
-            <p className="text-[26px] sm:text-3xl">Stories</p>
+            <p className="text-[26px] sm:text-4xl">Stories</p>
             <div className="flex gap-2 sm:gap-6 md:gap-24 sm:text-lg md:text-2xl md:tracking-wider">
               <p className={`py-2 px-3 bg-[${secondary}] rounded-md`}>
                 <Link to={"/home/mystories"}>My Stories</Link>
