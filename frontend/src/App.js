eb0d43a5fb85e044a6c601f0b3120ddad9ba4cda
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-09-21 00:49:33 +0530

    Nested Routing Improved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,28 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
-
+import StoriesPage from "./pages/StoriesPage";
+import MyStoriesPage from "./pages/MyStoriesPage";
+import Layout from "./components/Layout";
 
 function App() {
-  let location = useLocation();
+
   return (
     <div className="max-h-full max-w-[1920px] w-full">
-      {location.pathname === "/login" ||
-      location.pathname === "/signup" ? null : (
-        <Navbar />
-      )}
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="/">
-          <Route index element={<Navigate to="/home/stories" replace />} />
-          <Route path="home/*" element={<HomePage />}/>
-        </Route>
-        <Route path="*" element={<Navigate to="/home/stories" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/home" element={<Layout/>}>
+          <Route index element = {<Navigate to={'/home/stories'} replace/>}></Route>
+          <Route element={<HomePage/>}>
+            <Route path="stories" element={<StoriesPage />} />
+            <Route path="mystories" element={<MyStoriesPage />} />
+          </Route>
+        </Route>  
       </Routes>
     </div>
   );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import MyStoriesPage from './MyStoriesPage';
-import StoriesPage from './StoriesPage';
 import LeaderBoard from '../components/leaderboard/LeaderBoard';
+import { Outlet } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div className={`w-full grow gap-3 py-4 px-4 md:px-12 flex justify-between`}>
       <LeaderBoard/>
-      <Routes>
-        <Route path="stories" element={<StoriesPage />} />
-        <Route path="mystories" element={<MyStoriesPage />} />
-      </Routes>
+      <Outlet/>
     </div>
   );
 }
